c3a831fc79688bf326f4fc2f3dd5af198925ea80
Izumisea <111584400+Izumisea@users.noreply.github.com>
2023-02-04 21:31:28 +0800

    Update Bluetooth_test.ino

--- Bluetooth_test/Bluetooth_test.ino
+++ Bluetooth_test/Bluetooth_test.ino
@@ -1,28 +1,9 @@
-#include <Servo.h>
-
-Servo myservo;
 char t;
-int pos = 0;
-
-void pindot()
-  {
-  if(pos = 0; pos <= 180; pos += 1)
-   myservo.write(pos);              
-   delay(15);                       
-  if(pos = 180; pos >= 0; pos -= 1)  
-   myservo.write(pos);              
-   delay(15);                       
-  }
-  void top()
-  {
-  if(pos = 0; pos <= 180; pos += 1)
-   myservo.write(pos);                     
-  }
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
-  myservo.attach(9)
+
 }
 
 void loop() {
@@ -31,10 +12,5 @@ void loop() {
   t = Serial.read();
   Serial.println(t);
   }
-  if(t == 'N'){            //move forward(all motors rotate in forward direction)
-  pindot();
- }
-  if(t == 'F'){            //move forward(all motors rotate in forward direction)
-  top();
- }
+  
 }
